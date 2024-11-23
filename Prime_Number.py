2f29e77d1b47c11d9c00d592164b530a9a872173
Saja28 <116615043+Saja28@users.noreply.github.com>
2024-11-22 23:46:12 -0500

    Add files via upload

---
+++ Prime_Number.py
@@ -0,0 +1,34 @@
+import random
+n = 0 
+a= int(input("enter a number: \n"))
+c= 1
+b = a %  2
+b
+
+if b == 0 :
+    print(f"{a} is a even number.")
+else:
+    print (f" {a} is a odd number.")
+
+
+
+while n <= a  and c < a:
+        n += 1
+        c += 1  
+        f= a % c 
+           
+        if a==c :
+                 print (f"{a} is a prime number.") 
+        
+        elif f == 0 :   
+            print (f"{a} is  not a prime number.")  
+            break 
+        
+
+
+          
+              
+      
+
+
+
